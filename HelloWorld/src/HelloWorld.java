a369087d7224b7cf7db3223a8b30e121b52ae0a3
donalkelly <69033727+donalkelly@users.noreply.github.com>
2020-07-31 11:05:00 +0200

    Update HelloWorld.java

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		
 		System.out.println("hello world!");
 
-		// new branch testing
+		// merged to the main branch.
 		System.out.println("These are my new edits");
 	}
 
